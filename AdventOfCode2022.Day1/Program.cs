55ee73b6ef86414c1ba75aaa28bbc2df380a710e
Romeo di Gregorio <romeo_digregorio@hotmail.com>
2022-12-04 17:47:36 +0100

    Add project files.

---
+++ AdventOfCode2022.Day1/Program.cs
@@ -0,0 +1,24 @@
+﻿using AdventOfCode2022.Shared.Helpers;
+
+namespace AdventOfCode2022.Day1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var input = (new InputHelper()).GetInput();
+            SolvePart1(input);
+            SolvePart2(input);
+        }
+
+        private static void SolvePart2(string[] input)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void SolvePart1(string[] input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AdventOfCode2022.Shared/Helpers/InputHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022.Shared.Helpers;
+
+public class InputHelper
+{
+    private const string _fileName = "input.txt";
+    private const string _folderName = "Input";
+    public string InputFilePath { get; init; }
+
+    public InputHelper()
+    {
+        var executingAssemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? throw new Exception("Could not find executing assembly folder");
+
+        var currentProjectDirectory = Directory.GetParent(executingAssemblyFolder)!.Parent!.Parent;
+
+        InputFilePath = Path.Combine(currentProjectDirectory!.FullName, _folderName, _fileName) ?? throw new Exception("Could not find input file");
+
+        if (!File.Exists(InputFilePath))
+        {
+            throw new FileNotFoundException($"File not found: {InputFilePath}");
+        }
+    }
+
+    public string[] GetInput()
+    {
+        var input = File.ReadAllLines(InputFilePath);
+        return input ?? throw new Exception("Could not read input file");
+    }
+}
