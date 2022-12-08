2c80dd3387a6f1f3afb1874beae7b34b34c9a239
Romeo di Gregorio <romeo_digregorio@hotmail.com>
2022-12-09 00:11:54 +0100

    General folder rename+ add folder for day4

---
+++ Day4/Program.cs
@@ -0,0 +1,28 @@
+﻿using Shared.Helpers;
+
+namespace Day4;
+public static class Program
+{
+    private static void Main(string[] args)
+    {
+        var input = new InputHelper().GetInput();
+        SolvePart1(input);
+        SolvePart2(input);
+    }
+
+    private static void SolvePart1(string[] input)
+    {
+        var solution = App.SolvePart1(input);
+
+        var solutionMessage = $"The elf carrying most calories has {solution} calories.";
+        Console.WriteLine(solutionMessage);
+    }
+
+    private static void SolvePart2(string[] input)
+    {
+        var solution = App.SolvePart2(input);
+
+        var solutionMessage = $"The top three Elves total calories are {solution}";
+        Console.WriteLine(solutionMessage);
+    }
+}
